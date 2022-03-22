f3e8381a50d659daa9149fe8981fa40c322abf52
EC2 Default User <ec2-user@ip-172-31-1-81.ec2.internal>
2022-03-22 11:45:25 +0000

    ruby2

---
+++ dice.rb
@@ -0,0 +1,6 @@
+dice =   
+
+while dice != 6 do 
+  dice = rand(1..6)
+  puts dice
+end
---
+++ lesson7.rb
@@ -0,0 +1,7 @@
+puts "計算をはじめます"
+puts "2つの値を入力して下さい"
+input_key = gets
+input_key2 =gets
+puts "計算結果を出力します"
+puts "a*b=#{input_key.to_i*input_key2.to_i}"
+puts "計算を終了します"
